b431547def74fa882385b8008606e404b7955b48
phanthihiephs@gmail.com <phanthihiephs@gmail.com>
2020-06-23 01:38:19 +0700

    create banner screen home

---
+++ app/src/main/java/com/example/nguphaptienghan/activitys/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.nguphaptienghan.activitys
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nguphaptienghan.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
